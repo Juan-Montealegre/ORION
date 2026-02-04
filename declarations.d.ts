b6a503e3c5ba49e56d8cdec4e0a1681f965e937d
Juan Montealegre <juand-montealegreg@unilibre.edu.co>
2026-02-03 20:16:35 -0500

    PERSISTENCIA

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,1 +1,11 @@
 declare module '*.css';
+declare module '*.css';
+
+declare interface ImportMetaEnv {
+	readonly VITE_GEMINI_API_KEY: string;
+	// otras variables VITE_ que uses
+}
+
+declare interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
